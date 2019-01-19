0237cd6d5b7582908930db22684ce5192238f929
kaarimtareek <kaarimtareek@gmail.com>
2019-01-19 19:27:25 +0200

    The application

---
+++ plantx/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:plantx/my_planet/MyPlanet.dart';
+
+void main() => runApp(MyPlanet());
---
+++ plantx/lib/my_planet/MyPlanet.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class MyPlanet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.indigoAccent.shade200,
+        title: new Text(
+          "Planet X",
+          textDirection: TextDirection.ltr,
+        ),
+        centerTitle: true,
+        actions: <Widget>[],
+      ),
+      body: new Container(
+          child: new ListView(
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                'lib/my_planet/105planet.png',
+                width: 150.0,
+                height: 150.0,
+              ),
+              new Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              new PlanetWieght()
+            ],
+          )
+        ],
+      )),
+    ));
+  }
+}
+
+class PlanetWieght extends StatefulWidget {
+  _PlanetState createState() => _PlanetState();
+}
+
+class _PlanetState extends State<PlanetWieght> {
+  int radioVlaueGroup = 0;
+  double result=0.0;
+  String _currentPlanet = '';
+  void calcWieght(String s, double d) {
+    if(s.isNotEmpty){
+      if (int.parse(s) > 0) 
+          result = int.parse(s) * d;
+    }
+    _currentPlanet='Please Enter Wieght';
+  }
+  void onChangeText(String s){
+    setState(() {
+          switch (radioVlaueGroup) {
+        case 0:
+          calcWieght(_planetWieghtControl.text, 0.06);
+          _currentPlanet = 'Pluto';
+          break;
+        case 1:
+          calcWieght(_planetWieghtControl.text, 0.38);
+          _currentPlanet = 'Mars';
+          break;
+        case 2:
+          calcWieght(_planetWieghtControl.text, 0.91);
+          _currentPlanet = 'Venus';
+          break;
+      }
+        });
+  }
+
+  void handleRadioChangeValue(int value) {
+    setState(() {
+      radioVlaueGroup = value;
+      switch (radioVlaueGroup) {
+        case 0:
+          calcWieght(_planetWieghtControl.text, 0.06);
+          _currentPlanet = 'Pluto';
+          break;
+        case 1:
+          calcWieght(_planetWieghtControl.text, 0.38);
+          _currentPlanet = 'Mars';
+          break;
+        case 2:
+          calcWieght(_planetWieghtControl.text, 0.91);
+          _currentPlanet = 'Venus';
+          break;
+      }
+    });
+  }
+
+  final TextEditingController _planetWieghtControl =
+      new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 320.0,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new TextField(
+            onChanged:onChangeText  ,
+            controller: _planetWieghtControl,
+            keyboardType: TextInputType.number,
+            maxLengthEnforced: true,
+            maxLength: 8,
+            decoration: InputDecoration(
+                hintText: 'Enter Your Wieght on Earth',
+                icon: new Icon(Icons.person),
+                fillColor: Colors.indigoAccent.shade200),
+          ),
+          new Padding(padding: EdgeInsets.all(10.0)),
+          new Row(
+            children: <Widget>[
+              new Radio<int>(
+                value: 0,
+                groupValue: radioVlaueGroup,
+                onChanged: handleRadioChangeValue,
+              ),
+              Text('Pluto'),
+              new Radio<int>(
+                value: 1,
+                groupValue: radioVlaueGroup,
+                onChanged: handleRadioChangeValue,
+              ),
+              Text('Mars'),
+              new Radio<int>(
+                value: 2,
+                groupValue: radioVlaueGroup,
+                onChanged: handleRadioChangeValue,
+              ),
+              Text('Venus')
+            ],
+          ),
+          new Padding(
+            padding: EdgeInsets.all(15.0),
+          ),
+          //Result Text
+          new Text(
+              "Your Wieght on $_currentPlanet is ${result.toStringAsFixed(3)}"
+              )
+        ],
+      ),
+    );
+  }
+}
